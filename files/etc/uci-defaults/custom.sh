9b20c8afc178e27dc61a384388b9ebd22e8116d2
caddaric <124473165+caddaric9527@users.noreply.github.com>
2024-12-31 09:03:51 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -35,7 +35,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by caddaric9527"
+NEW_DESCRIPTION="Compiled by Caddaric"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
